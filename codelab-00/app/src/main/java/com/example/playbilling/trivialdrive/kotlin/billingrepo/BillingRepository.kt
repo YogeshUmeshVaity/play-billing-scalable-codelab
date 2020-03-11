c62c50f9d791986ecead4e213b73f41863440391
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-03-11 21:09:31 +0530

    Add CachedPurchase, PurchaseDao, AugmentedSkuDetails, and AugmentedSkuDetailsDao, Entitlement, EntitlementsDao, and LocalBillingDb.

---
+++ codelab-00/app/src/main/java/com/example/playbilling/trivialdrive/kotlin/billingrepo/BillingRepository.kt
@@ -0,0 +1,2 @@
+package com.example.playbilling.trivialdrive.kotlin.billingrepo
+
---
+++ codelab-00/app/src/main/java/com/example/playbilling/trivialdrive/kotlin/billingrepo/localdb/LocalBillingDb.kt
@@ -0,0 +1,28 @@
+package com.example.playbilling.trivialdrive.kotlin.billingrepo.localdb
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [CachedPurchase::class, GasTank::class, PremiumCar::class, GoldStatus::class, AugmentedSkuDetails::class],
+        version = 1, exportSchema = false)
+abstract class LocalBillingDb : RoomDatabase() {
+    abstract fun purchaseDao(): PurchaseDao
+    abstract fun entitlementsDao(): EntitlementsDao
+    abstract fun skuDetailsDao(): AugmentedSkuDetailsDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: LocalBillingDb? = null
+
+        fun getInstance(context: Context): LocalBillingDb = INSTANCE?: synchronized(this) {
+            INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    LocalBillingDb::class.java,
+                    "purchase_db")
+                    .fallbackToDestructiveMigration()//remote sources more reliable
+                    .build().also { INSTANCE=it }
+        }
+    }
+}
