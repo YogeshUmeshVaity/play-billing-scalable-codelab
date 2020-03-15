3f17d4a7bd7c7702f48495a4456a54de13416b31
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-03-15 20:04:55 +0530

    Add SKU IDs.

--- codelab-00/app/src/main/java/com/example/playbilling/trivialdrive/kotlin/billingrepo/BillingRepository.kt
+++ codelab-00/app/src/main/java/com/example/playbilling/trivialdrive/kotlin/billingrepo/BillingRepository.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import android.content.Context
 import android.util.Log
 import com.android.billingclient.api.*
+import com.example.playbilling.trivialdrive.kotlin.billingrepo.BillingRepository.GameSku.CONSUMABLE_SKUS
+import com.example.playbilling.trivialdrive.kotlin.billingrepo.BillingRepository.GameSku.INAPP_SKUS
+import com.example.playbilling.trivialdrive.kotlin.billingrepo.BillingRepository.GameSku.SUBS_SKUS
 import kotlinx.coroutines.*
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
@@ -110,5 +113,34 @@ class BillingRepository private constructor(private val application: Application
                 apply()
             }
         }
+    }
+
+    /**
+     * [INAPP_SKUS], [SUBS_SKUS], [CONSUMABLE_SKUS]:
+     *
+     * Where you define these lists is quite truly up to you. If you don't need
+     * customization, then it makes since to define and hardcode them here, as
+     * I am doing. Keep simple things simple. But there are use cases where you may
+     * need customization:
+     *
+     * - If you don't want to update your APK (or Bundle) each time you change your
+     *   SKUs, then you may want to load these lists from your secure server.
+     *
+     * - If your design is such that users can buy different items from different
+     *   Activities or Fragments, then you may want to define a list for each of
+     *   those subsets. I only have two subsets: INAPP_SKUS and SUBS_SKUS
+     */
+
+    private object GameSku {
+        val GAS = "gas"
+        val PREMIUM_CAR = "premium_car"
+        val GOLD_MONTHLY = "gold_monthly"
+        val GOLD_YEARLY = "gold_yearly"
+
+        val INAPP_SKUS = listOf(GAS, PREMIUM_CAR)
+        val SUBS_SKUS = listOf(GOLD_MONTHLY, GOLD_YEARLY)
+        val CONSUMABLE_SKUS = listOf(GAS)
+        //coincidence that there only gold_status is a sub
+        val GOLD_STATUS_SKUS = SUBS_SKUS
     }
 }
